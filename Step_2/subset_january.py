b9dccdd7eb620bfd8a63db8c05f5633d34468c57
Poorna Kumar <poorna.kumar@gmail.com>
2016-10-14 11:17:47 -0700

    Take care of carriage returns

--- Step_2/subset_january.py
+++ Step_2/subset_january.py
@@ -3,13 +3,13 @@ import csv
 
 doc = csv.writer(sys.stdout, lineterminator='\n')
 
-header = sys.stdin.readline().split(",")
-header = [x.strip(' ') for x in header]
+header = sys.stdin.readline().strip().split(",")
+header = [x.strip() for x in header]
 date_index = header.index("pickup_datetime") 
 doc.writerow(header)
 
 for line in sys.stdin:
-    line = line.split(",")
-    line = [x.strip(' ') for x in line]
+    line = line.strip().split(",")
+    line = [x.strip() for x in line]
     if line[date_index][0:10] == "2013-01-15":
         doc.writerow(line)
