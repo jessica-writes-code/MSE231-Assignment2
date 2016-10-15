6edd2c1b8da5dac2466406ae0188a63238e8d651
jmoore523 <jmoore523@gmail.com>
2016-10-14 17:17:01 -0700

    Readme and comments in script

--- README.txt
+++ README.txt
@@ -13,3 +13,19 @@ subset_january.py
         In order to obtain both subsetted trip data and subsetted fare data,
         we ran this script separately on each of the provided January data sets
 
+join_map.py and join_reduce.py
+    Perform the map and and reduce steps (respectively) to join trip and fare data.
+    Input:
+        At least two CSV files with the same form as those located at 
+        https://5harad.com/data/nyctaxi/2013_trip_data_1.csv.gz and 
+        https://5harad.com/data/nyctaxi/2013_trip_fare_1.csv.gz
+        There should be at least one CSV file of the "trip_data" format
+        and one CSV file of the "trip_fare" format. Ideally, these should have 
+        records which match on medallion, hack license, and pickup_datetime.
+    Output
+        Tab-separated records which include both trip and fare information for 
+        all matching records (i.e., those appearing in both files).
+    Notes:
+        The reduce step performs filtering for obviously erroneous data. 
+        See the corresponding report for additional details.
+
--- Step_3/join_reduce.py
+++ Step_3/join_reduce.py
@@ -2,42 +2,47 @@
 
 import sys
 
+## Function to filter erroneous data
 def passesIntegrityCheck(line_to_write):
     try:
-        #flag if fare total is less than $3
+        # Fare total is less than $2.50
         totalFare = float(line_to_write['1'][10])
         if totalFare < 2.50:
             return False
+        # Total passengers is less than or equal to 0
         totalPassengers = int(line_to_write['2'][7])
         if totalPassengers <= 0:
             return False
+        # Trip time is less than or equal to 0
         tripTime = int(line_to_write['2'][8])
         if tripTime <= 0:
             return False
+        # Trip distance is less than or equal to 0
         tripDistance = float(line_to_write['2'][9])
         if tripDistance <= 0:
             return False
         return True
     except ValueError:
+        # One of the above features is unreadable
         return False
     
-#Assume input is sorted by key
+## Assume input is sorted by key
 if __name__ == "__main__":
     currKey = ""
     lastKey = ""
-    line_to_write = {} #dictionary going from tableID to table contents
+    line_to_write = {} # dictionary going from tableID to table contents
    
     for line in sys.stdin:
         tokens = line.strip().split('\t')
         tokens = [token.strip() for token in tokens]
         currKey = tokens[0]
         currTable = tokens[1]
-        if currKey != lastKey: #if the key changed (regardless of whether the table is full or not
-            line_to_write = {} #flush the dictionary, make space for (new) current key
+        if currKey != lastKey: # if the key changed (regardless of whether the table is full or not
+            line_to_write = {} # flush the dictionary, make space for (new) current key
         lastKey = currKey
-        if len(line_to_write) == 2: #if three consecutive keys are the same
+        if len(line_to_write) == 2: # if three consecutive keys are the same
             continue        
-        if len(line_to_write) == 1:
+        if len(line_to_write) == 1: # if one table's info has been collected, but not the other
             storedTable = line_to_write.keys()[0]
             if storedTable == currTable:
                 continue
@@ -48,5 +53,5 @@ if __name__ == "__main__":
                     line2 = '\t'.join(line_to_write['2'])
                     lineToPrint = '\t'.join([currKey, line1, line2])
                     print lineToPrint
-        elif len(line_to_write) == 0:
+        elif len(line_to_write) == 0: # if neither table's info has been collected 
             line_to_write[currTable] = tokens[2:]
