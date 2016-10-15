b10933ceb65afb2cf220ad9e301fca072b718a67
jmoore523 <jmoore523@gmail.com>
2016-10-14 17:18:05 -0700

    More comments

--- Step_3/join_map.py
+++ Step_3/join_map.py
@@ -8,13 +8,14 @@ for line in sys.stdin:
     tokens = [token.strip() for token in tokens]
     line_to_write = []
     #fare data: 11 fields
-    #trip data: 14 fields
     if len(tokens) == 11:
         key = '_'.join([tokens[0], tokens[1], tokens[3]]).replace(' ', '_') #replace all spaces in the key with underscores
-        tableID = '1'         
+        tableID = '1'  
+    #trip data: 14 fields       
     elif len(tokens) == 14:
         key = '_'.join([tokens[0], tokens[1], tokens[5]]).replace(' ', '_') #replace all spaces in the key with underscores
         tableID = '2'
+    #unreadable data from either file
     else:
         print >> sys.stderr, line.strip()
         continue
